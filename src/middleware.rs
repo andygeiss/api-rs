f19b253b50637ecf8330e664854a74e3b17d8647
andygeiss <4ndygeiss@gmail.com>
2024-07-16 08:58:35 +0200

    refactor: clean up extension

--- src/middleware.rs
+++ src/middleware.rs
@@ -5,23 +5,16 @@ use axum::{
     middleware::Next,
     response::Response,
 };
-use serde::Deserialize;
 
-#[derive(Clone, Deserialize)]
-pub struct Token {
-    pub value: String,
-}
-
-pub async fn authorize(mut req: Request, next: Next) -> Result<Response, StatusCode> {
+pub async fn authorize(req: Request, next: Next) -> Result<Response, StatusCode> {
     // Extract authorization header
     if let Some(hv) = req.headers().get(header::AUTHORIZATION) {
         let value = hv.to_str().unwrap().to_string();
         // Check the token
         let (_, suffix) = value.split_at(7);
         if security::token::is_valid(suffix.to_string()) {
-            req.extensions_mut().insert(Token { value });
+            return Ok(next.run(req).await);
         }
-        return Ok(next.run(req).await);
     }
     // Block API requests without a token
     Err(StatusCode::UNAUTHORIZED)
